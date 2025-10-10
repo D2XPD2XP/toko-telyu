1b4ea52f150b466424b005b328a261511bc53e51
D2XPD2XP <derildzxp222@gmail.com>
2025-10-11 00:38:22 +0700

    adding splash screen

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget{
+  const Homepage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _Homepage();
+  }
+}
+
+class _Homepage extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
